59c78a9248c4ee3cdf01e3a1ac8d61687f312298
David Pilato <david+github@pilato.fr>
2011-08-03 10:15:33 +0200

    Trying to solve classloading issues

--- src/main/java/fr/pilato/maven/elasticsearch/ElasticSearchMojo.java
+++ src/main/java/fr/pilato/maven/elasticsearch/ElasticSearchMojo.java
@@ -18,29 +18,34 @@ package fr.pilato.maven.elasticsearch;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.MalformedURLException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
-import org.elasticsearch.osem.core.ObjectContext;
-import org.elasticsearch.osem.core.ObjectContextFactory;
+import org.codehaus.classworlds.ClassRealm;
+import org.codehaus.classworlds.ClassWorld;
+import org.codehaus.classworlds.DuplicateRealmException;
+import org.elasticsearch.osem.annotations.Searchable;
+import org.elasticsearch.osem.annotations.impl.AttributeSourceImpl;
 import org.elasticsearch.osem.core.ObjectContextMapper;
-import org.elasticsearch.osem.core.ObjectContextMappingException;
+import org.elasticsearch.osem.core.impl.ObjectContextMapperImpl;
+import org.elasticsearch.osem.property.impl.PropertySignatureSourceImpl;
 
 /**
  * Goal generation JSON mapping files from java class
  * 
  * @goal create
  * @phase package
- * @requiresDependencyResolution compile
  */
 public class ElasticSearchMojo extends AbstractMojo {
-
-	private ObjectContext context;
 	private ObjectContextMapper mapper;
-	// private AttributeSource as ;
-	// private PropertySignatureSource ass;
 
 	/**
 	 * Indicates all classes that you need to create mapping
@@ -49,23 +54,66 @@ public class ElasticSearchMojo extends AbstractMojo {
 	private String[] classes;
 
     /**
-     * The maven project
-     * 
-     * @parameter expression="${project}"
-     * @required
-     * @readonly
-     */
-    private MavenProject project;
-    
-    /**
      * Output Directory for generating mapping files
      * @parameter default-value="${project.build.directory}/elasticsearch"
      */
     private String outputDirectory;
 
+    /**
+     * Maven project
+     * @parameter expression="${project}"
+     * @readonly
+     * @required
+     */
+    private MavenProject project;
+    
+    /**
+     * Dépendances du projet Maven qui utilise le plugin
+     * @parameter expression="${project.compileArtifacts}"
+     */
+    private List<Artifact> projectArtifacts;
+    
+    /**
+     * Dépendances du plugin lui-même
+     * @parameter expression="${pluginArtifacts}"
+     */
+    private List<Artifact> pluginArtifacts;
+    
+    /**
+     * Input Directory for classes files (.class)
+     * @parameter default-value="${project.build.outputDirectory}"
+     */
+    private String inputDirectory;
+
+    protected ClassLoader createClassLoader()
+    	throws MalformedURLException, DuplicateRealmException {
+    	ClassWorld world = new ClassWorld();
+    	ClassRealm realm = world.newRealm( "elasticsearch-plugin" );
+    	
+    	// Ajout de tous les éléments du classpath "compile" du projet
+		getLog().debug( "Adding project compile dependencies to classpath :");
+    	for (Artifact artifact : projectArtifacts ) {
+    		getLog().debug( "  - " + artifact.toString());
+    		realm.addConstituent( artifact.getFile().toURL() );
+    	}
+
+    	// Ajout des dépendances du plugin
+		getLog().debug( "Adding plugin dependencies to classpath :");
+    	for (Artifact artifact : pluginArtifacts ) {
+    		getLog().debug( "  - " + artifact.toString());
+    		realm.addConstituent( artifact.getFile().toURL() );
+    	}
+
+    	// Ajout des répertoires de compilation
+		getLog().debug( "Adding build path project to classpath :");
+		realm.addConstituent( new File(inputDirectory).toURL() );
+		getLog().debug( "  - " + inputDirectory);
+    	return realm.getClassLoader();
+    }
+    
     @Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		File JSONFile = null;
+        File JSONFile = null;
 
 		getLog().info("Starting ElasticSearch Plugin " + outputDirectory);
 		if (classes == null || classes.length == 0) {
@@ -83,78 +131,68 @@ public class ElasticSearchMojo extends AbstractMojo {
 				getLog().debug("Output directory " + outputDirectory + " created");
 			}
 
-			for (String classname : classes) {
-				try {
-					getLog().debug("Generating mapping for " + classname);
-					String mappingFileName = classname + ".json";
-					
-					getLog().info(project.toString());
-					//project.ge
-					Class<?> _class = Class.forName(classname);
-					
-					getLog().info("" + mappingFileName);
-//					getLog().info("" + mappingFileName + " content : " + translate(_class));
-					
-//					JSONFile = new File("./ES_JsonMapping/" + "es/" + classname
-//							+ ".json");
-//					JSONFile.createNewFile();
-//					JSONFile.setWritable(true);
-//					
-//					FileWriter fw = new FileWriter("./ES/Mapping/" + classname
-//							+ ".json");
-//					fw.write(translate(Class.forName(classname)));
-//					fw.close();
+			try {
+				// Creating mapper for OSEM
+		        mapper = new ObjectContextMapperImpl( 
+		        		new AttributeSourceImpl(), 
+		        		new PropertySignatureSourceImpl());
 
-//				} catch (IOException e) {
-//					e.printStackTrace();
-				} catch (ClassNotFoundException e) {
-					getLog().error("Can not generate mapping - ClassNotFoundException : " + e.getMessage());
-				} finally {
-
+				// Getting the right classloader
+				ClassLoader cl = createClassLoader();
+				
+//		        String _classname = "fr.pilato.maven.elasticsearch.pojo.Tweet";
+//		        
+//		        // cl.loadClass(_classname);
+//				Class<?> __class = Class.forName(_classname);
+//		        
+////				Class<?> __class = Class.forName(_classname, false, cl);
+//		        mapper.add(__class);
+//				String _result = new String(mapper.getMapping(__class).copiedBytes());
+//				getLog().warn(_result);
+				
+				// Step 1 : setup the OSEM context by loading all needed classes
+				for (String classname : classes) {
+					getLog().debug("Adding " + classname + " to OSEM context");
+//					Class<?> _class = Class.forName(classname);
+					Class<?> _class = Class.forName(classname, true, cl);
+					
+					Annotation annos[] = _class.getAnnotations();
+					Annotation anno = _class.getAnnotation(Searchable.class);
+					
+					for (int i = 0; i < annos.length; i++) {
+						Class<?> __class = annos[i].annotationType();
+						if (__class.equals(Searchable.class)) {
+							getLog().debug("   ***c** BINGO ********");
+						}
+					}
+					
+					getLog().debug("DONE");
+			        mapper.add(_class);
 				}
-			}
 			
+				// Step 2 : call OSEM mapping generation for all classes
+				for (String classname : classes) {
+					String mappingFileName = classname + ".json";
+					getLog().debug("Generating mapping : " + mappingFileName);
+//					Class<?> _class = Class.forName(classname);
+					Class<?> _class = Class.forName(classname, true, cl);
+					
+					String result = new String(mapper.getMapping(_class).copiedBytes());
+					
+					getLog().info(result);
+				}
+			} catch (ClassNotFoundException e) {
+				getLog().error("Can not generate mapping - ClassNotFoundException : " + e.getMessage());
+			} catch (MalformedURLException e) {
+				getLog().error("Can not generate mapping - MalformedURLException : " + e.getMessage());
+			} catch (DuplicateRealmException e) {
+				getLog().error("Can not generate mapping - DuplicateRealmException : " + e.getMessage());
+			} catch (IOException e) {
+				getLog().error("Can not generate mapping - IOException : " + e.getMessage());
+			} finally {
+
+			}
 		}
-	}
-
-	public String translate(Class<?> c) {
-
-		context = ObjectContextFactory.create();
-		mapper = context.add(c);
-		mapper.add(c);
-
-		// public ObjectContextMapperImpl(AttributeSource attributes,
-		// PropertySignatureSource signatures) {
-		// mapper = new ObjectContextMapperImpl(as.getProperties(c),
-		// ass.get(c));
-
-		String JSONformat = "";
-
-		try {
-			// mapper = new ObjectContextMapperImpl(new AttributeSourceImpl(),
-			// new PropertySignatureSourceImpl());
-			// mapper.add(c.getClass());
-
-			// mapper = new ObjectContextMapperImpl(new AttributeSourceImpl(),
-			// new PropertySignatureSourceImpl());
-			// JSONformat = new
-			// String(mapper.getMapping(c.getClass()).copiedBytes());
-
-			JSONformat = new String(mapper.getMapping(c).copiedBytes());
-			// mapper.add(Dossier.class);
-			// System.out.println("Classe dossier mappee ? : " +
-			// mapper.isRegistred(Dossier.class));
-			// System.out.println("chaine JSONString : " + JSONString);
-			// JSONString = new
-			// String(mapper.getMapping(Dossier.class).copiedBytes());
-		} catch (ObjectContextMappingException e) {
-			System.err.println("Exception : " + e.getMessage());
-		} catch (IOException e) {
-			System.err.println("Exception : " + e.getMessage());
-		} finally {
-
-		}
-		return JSONformat;
 	}
 
 }
